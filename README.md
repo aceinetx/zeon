2f1c2ebfe8ab0d8945c9bcedfd8f7f9f22b1d1de
aceinetx <mouselin82@gmail.com>
2025-07-26 19:27:04 +0000

    Refactor

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A web browser made with cef & imgui
 
 References
 - https://github.com/gotnospirit/cef3-sdl2/
+- https://github.com/jamethy/sdl2-cef
--- src/zeon_kb.hh
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SDL3/SDL.h>
-
-namespace z {
-int getKeyboardModifiers(uint16_t const mod);
-int getWindowsKeyCode(SDL_KeyboardEvent const& key);
-} // namespace z
